2ee31a0ecc6830730148fbf44c5c6031da228677
DaniyalNobody <daniyalfaisal15@gmail.com>
2023-12-04 03:21:18 +0500

    initial commit

---
+++ Core/Interfaces/IStudentSubjectsBL.cs
@@ -0,0 +1,20 @@
+﻿using Core.Models.RequestModels;
+using Core.Models.ResponseModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core.Interfaces
+{
+    public interface IStudentSubjectsBL
+    {
+        public StudentSubjectsResponseDto SaveStudentSubjects(StudentSubjectsRequestDto studentSubjectsRequestDto);
+        public StudentSubjectsResponseDto DeleteStudentSubjects(int Id);
+        public StudentSubjectsResponseDto UpdateStudentSubjects(int Id, StudentSubjectsRequestDto studentSubjectsRequestDto);
+        
+
+
+    }
+}
---
+++ Core/Models/Student.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core.Models
+{
+    public interface IStudent
+    {
+        string Name { get; set; }
+        string RollNo { get; set; }  
+
+        string PhoneNumber { get; set; } 
+    }
+}
